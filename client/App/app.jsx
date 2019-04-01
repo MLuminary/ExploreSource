57f049495b0bf3a9f2bd86b6629f59349a11b4c9
Hutchins <418037025@qq.com>
2019-04-01 22:36:34 +0800

    feat: add tabs

--- client/App/app.jsx
+++ client/App/app.jsx
@@ -1,6 +1,4 @@
 import * as React from 'react'
-import { Link } from 'react-router-dom'
-import Button from '@material-ui/core/Button'
 import CssBaseline from '@material-ui/core/CssBaseline'
 
 import Routes from '../config/router'
@@ -19,12 +17,6 @@ export default class App extends React.Component {
     return [
       <div key="app">
         <ButtonAppBar />
-        <Link to="/">首页</Link>
-        <br />
-        <Link to="/detail">详情</Link>
-        <Button variant="contained" color="primary">
-          你好，世界
-        </Button>
       </div>,
       <Routes key="routes" />,
       <CssBaseline />
--- client/views/topic-list/topic-list.jsx
+++ client/views/topic-list/topic-list.jsx
@@ -1,3 +1,63 @@
 import * as React from 'react'
+import * as PropTypes from 'prop-types'
+import { observer, inject } from 'mobx-react'
+import Helmet from 'react-helmet'
+import { Tabs, Tab } from '@material-ui/core'
 
-export const TopicList = () => (<div>this is TopicList</div>)
+import Container from '../layout/container'
+import AppState from '../../store/app-state'
+
+@inject('appState') @observer
+export class TopicList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tabIndex: 0
+    }
+  }
+
+
+  componentDidMount() {
+    // do
+  }
+
+  changeTab = (e, index) => {
+    this.setState({ tabIndex: index })
+  }
+
+  // 服务端调用 asyncBootstrap 时会在此同时执行此方法
+  // 名字必须为 bootstrap
+  bootstrap() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const { appState } = this.props
+        appState.count = 3
+        resolve(true)
+      })
+    })
+  }
+
+  render() {
+    const { tabIndex } = this.state
+    return (
+      <Container>
+        <Helmet>
+          <title>This is topic list</title>
+          <meta name="description" content="This is description" />
+        </Helmet>
+        <Tabs onChange={this.changeTab} value={tabIndex}>
+          <Tab label="全部" />
+          <Tab label="精华" />
+          <Tab label="分享" />
+          <Tab label="问答" />
+          <Tab label="招聘" />
+          <Tab label="测试" />
+        </Tabs>
+      </Container>
+    )
+  }
+}
+
+TopicList.propTypes = {
+  appState: PropTypes.instanceOf(AppState)
+}
